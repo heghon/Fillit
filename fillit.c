6ac4c947395c5d41d738fc191ac668eedf292c47
Benjamin PRUNEVIELLE <bprunevi@e1r2p7.42.fr>
2019-01-14 14:14:45 +0100

    fillit.o uses file in argv[1]

--- fillit.c
+++ fillit.c
@@ -6,26 +6,25 @@
 /*   By: bprunevi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/14 12:27:44 by bprunevi          #+#    #+#             */
-/*   Updated: 2019/01/14 14:07:21 by bprunevi         ###   ########.fr       */
+/*   Updated: 2019/01/14 14:13:24 by bprunevi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-int		main(int fd, char **argv)
+int		main(int fd, char *argv[1])
 /* Teste si tous les tetrominos de 'test_valid' sont valides.
  * Renvoie '-1' dans le cas contraire.
  */
 {
 	char tetromino[21];
-	int file;
 
-	file = open("test_valid", O_RDONLY);
-	while (read(file,tetromino,21) > 0)
+	fd = open(argv[1], O_RDONLY);
+	while (read(fd,tetromino,21) > 0)
 	{
 		if (!is_valid(tetromino))
 			exit(-1);
 		ft_putstr("A valid tetromino was detected !\n");
 	}
-	close(file);
+	close(fd);
 }
