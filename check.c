63976d7fd6f5f5bac99efa30ccd97ab91b698b00
Benjamin PRUNEVIELLE <bprunevi@e3r6p3.42.fr>
2019-01-15 15:31:26 +0100

    Added str to struct tetromino conversion

--- check.c
+++ check.c
@@ -6,11 +6,13 @@
 /*   By: bprunevi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/14 13:29:02 by bprunevi          #+#    #+#             */
-/*   Updated: 2019/01/14 13:55:54 by bprunevi         ###   ########.fr       */
+/*   Updated: 2019/01/15 15:20:58 by bprunevi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "fillit.h"
+#ifndef CHECK
+# define CHECK
+# include "fillit.h"
 
 int		four_connections(char *str)
 /* Prends chacun des 4 '#' un a un et verifie qu'ils ont une
@@ -53,3 +55,137 @@ int		is_valid(char *str)
 				b++;
 	return (str[a] == '\n' && b == 4 && a == 20 && four_connections(str));
 }
+t_tetri	format_tetri(char *str, char id)
+/* TROP LONG POUR LA NORME ! (mais ca se change)
+ * Prend un tetromino VALIDE et rend une structure tetromio correspondante.
+ * 'id' est la lettre correspondant au tetri (par exemple 'A')
+ */
+{
+	char	rtn[6][4];
+	int		i;
+	int		j;
+	int		x;
+	t_tetri tt;
+
+	i = -1;
+	x = 0;
+	tt.h = 0;
+	tt.l = 0;
+	while (++i <= 3 && (j = -1) && (rtn[4][i] = '.'))
+		while (++j <= 3 && (rtn[i][j] = str[i * 5 + j]))
+			if (rtn[4][i] != '#' && rtn[i][j] == '#' && ++tt.h)
+				rtn[4][i] = '#';
+	j = -1;
+	while (++j <= 3 && (i = -1) && (rtn[5][j] = '.'))
+		while (++i <= 3)
+			if (rtn[5][j] != '#' && rtn[i][j] == '#' && ++tt.l)
+				rtn[5][j] = '#';
+	if (!(tt.str = malloc(tt.h * tt.l + 1))) //+1 a retirer (LE STRING NE DOIT PAS ETRE TERMINE PAR \0)
+		exit (-1);
+	tt.str[tt.l * tt.h] = '\0';// A RETIRER - LE STRING NE DOIT PAS ETRE TERMINE PAR \0
+	i = -1;
+	while (++i <= 3 && (j = -1))
+		while (++j <= 3)
+			if (rtn[4][i] == '#' && rtn[5][j] == '#')
+				tt.str[x++] = rtn[i][j] == '#' ? id : '.';
+	return (tt);
+}
+/*
+char	**format(char *str)
+{
+	char	**rtn;
+	int		i;
+	int		j;
+	
+	i = 0;
+	j = 0;
+	if (!(rtn = malloc(sizeof(char *) * 4)))
+		exit (-1);
+	while (i <= 4)
+	{
+		if (!(rtn[i] = malloc(4)))
+				exit (-1);
+		while (j++ <= 4)
+			rtn[i][j - 1] = str[i * 5 + (j - 1)];
+		i++;
+	}
+	return (rtn);
+}
+void	format_tetri(char *str) // FINALE (TROP LONGUE)
+{
+	char	rtn[6][4];
+	int		i;
+	int		j;
+	int		x;
+	t_tetri tt;
+	
+	i = -1;
+	x = 0;
+	tt.h = 0;
+	tt.l = 0;
+	while (++i <= 3 && (j = -1) && (rtn[4][i] = '.'))
+		while (++j <= 3 && (rtn[i][j] = str[i * 5 + j]))
+			if (rtn[4][i] != '#' && rtn[i][j] == '#' && ++tt.h)
+				rtn[4][i] = '#';
+	j = -1;
+	while (++j <= 3 && (i = -1) && (rtn[5][j] = '.'))
+		while (++i <= 3)
+			if (rtn[5][j] != '#' && rtn[i][j] == '#' && ++tt.l)
+				rtn[5][j] = '#';
+	if (!(tt.final = malloc(tt.h * tt.l + 1)))
+		exit (-1);
+	tt.final[tt.l * tt.h] = '\0';
+	i = -1;
+	while (++i <= 3 && (j = -1))
+		while (++j <= 3)
+			if (rtn[4][i] == '#' && rtn[5][j] == '#')
+				tt.final[x++] = rtn[i][j];
+	ft_putnbr(tt.h);
+	ft_putchar(':');
+	ft_putnbr(tt.l);
+	ft_putchar('\n');
+	ft_putstr(tt.final);
+	ft_putstr("\n\n");
+}
+void	format_tetri(char *str)
+{
+	char	rtn[6][4];
+	char	*final;
+	int		i;
+	int		j;
+	int		h;
+	int		l;
+	
+	i = -1;
+	h = 0;
+	l = 0;
+	while (++i <= 3 && (j = -1) && (rtn[4][i] = '.'))
+		while (++j <= 3 && (rtn[i][j] = str[i * 5 + j]))
+			if (rtn[4][i] != '#' && rtn[i][j] == '#' && ++h)
+				rtn[4][i] = '#';
+	j = -1;
+	while (++j <= 3 && (i = -1) && (rtn[5][j] = '.'))
+		while (++i <= 3)
+			if (rtn[5][j] != '#' && rtn[i][j] == '#' && ++l)
+				rtn[5][j] = '#';
+	i = -1;
+	if (!(final = malloc(h * l)))
+		exit (-1);
+	while (++i <= 3 && (j = -1))
+		while (++j <= 3)
+			if (rtn[4][i] == '#' && rtn[5][j] == '#')
+				final[i * l + j] = rtn[i][j];
+	i = -1;
+	while (++i <= 5 && (j = -1))
+	{
+		while (++j <= 3)
+			ft_putchar(rtn[i][j]);
+		ft_putchar('\n');
+	}
+	ft_putnbr(h);
+	ft_putchar(':');
+	ft_putnbr(l);
+	ft_putchar('\n');
+}
+*/
+#endif
--- fillit.c
+++ fillit.c
@@ -6,7 +6,7 @@
 /*   By: bprunevi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/14 12:27:44 by bprunevi          #+#    #+#             */
-/*   Updated: 2019/01/14 14:13:24 by bprunevi         ###   ########.fr       */
+/*   Updated: 2019/01/15 15:24:24 by bprunevi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,18 @@ int		main(int fd, char *argv[1])
  * Renvoie '-1' dans le cas contraire.
  */
 {
-	char tetromino[21];
+	char	tetromino[21];
+	t_tetri	tab_tetromino[26];
+	int		i;
 
 	fd = open(argv[1], O_RDONLY);
+	i = 0;
 	while (read(fd,tetromino,21) > 0)
 	{
 		if (!is_valid(tetromino))
 			exit(-1);
-		ft_putstr("A valid tetromino was detected !\n");
+		//Example d'utilisation :
+		tab_tetromino[0] = format_tetri(tetromino, 'A');
 	}
 	close(fd);
 }
