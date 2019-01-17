68668e8aca393f0a7b4b228791e9c8cbb118504e
Benjamin PRUNEVIELLE <bprunevi@e1r4p6.42.fr>
2019-01-17 12:53:34 +0100

    Mise a niveau de check.c

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: bprunevi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/14 11:55:59 by bprunevi          #+#    #+#             */
-/*   Updated: 2019/01/15 15:24:22 by bprunevi         ###   ########.fr       */
+/*   Updated: 2019/01/17 12:53:03 by bprunevi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,11 @@
 
 typedef struct       s_tetri
 {
-   int            h;
-   int            l;
-   char           *str;
-   struct s_tetri *next;
-}                    t_tetri;
+   int				h;
+   int				l;
+   char				*str;
+   struct s_tetri	*next;
+}					t_tetri;
 
 # include "check.c"
 #endif
