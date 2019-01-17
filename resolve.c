0128fa4afeb20eb7ee3a8f37b900f24f6a256c1a
Baptiste MENANT <bmenant@student.42.fr>
2019-01-17 17:46:19 +0100

    MaJ de resolve

--- resolve.c
+++ resolve.c
@@ -6,7 +6,7 @@
 /*   By: bmenant <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/14 13:28:30 by bmenant           #+#    #+#             */
-/*   Updated: 2019/01/17 16:12:36 by bmenant          ###   ########.fr       */
+/*   Updated: 2019/01/17 17:43:45 by bmenant          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,19 @@ int		verification(t_tetri *tetri, int side, char *final_str, int s)
 	i = 0;
 	if (s == side * side)
 		return (-1);
-	if ((((s % 4) + tetri->l) / 4) > 0 || (((s / 4) + tetri->h) / 4) > 0)
-		return (0);
 	while (tetri->str)
 	{
-		if (final_str[a] != '.' && tetri->str[i++] != '.')
+		if (final_str[s] != '.' && tetri->str[i++] != '.')
 			return (0);
-		if (a == ((a % 4) + tetri->l))
-			a += (side - tetri->l);
+		if (s == ((s % 4) + tetri->l))
+			s += side;
 		else
-			a++;
+			s++;
 	}
 	return (1);
 }
 
-int		placement(t_tetri *tetri, int side, char *final_str, int s)
+void	placement(t_tetri *tetri, int side, char *final_str, int s)
 {
 	int		i;
 
@@ -48,29 +46,32 @@ int		placement(t_tetri *tetri, int side, char *final_str, int s)
 	}
 }
 
-char	*resolve(t_tetri *tetri, int side)
+char	*resolve(t_tetri *tetri, int side, int start)
+	/* on doit commencer start a 0 et side a 2 */
 {
 	char		*final_str;
-	int			s;
+//	int			s;
 	int			ret;
 	t_tetri		*backup;
 
+	if (start = side * side)
+		return (resolve(tetri, ++side, 0));
 	final_str = ft_strnew((size_t)(side * side));
 	final_str = ft_memset(final_str, '.', (side * side));
 	backup = tetri;
-	s = 0;
+//	start = 0;
 	while (tetri->str)
 	{
-		if (ret = verification(tetri, side, final_str, s) == 0)
-			s++;
+		if (ret = verification(tetri, side, final_str, start) == 0)
+			start++;
 		else if (ret == -1)
 		{
 			free(final_str);
-			return (resolve(backup, ++side));
+			return (resolve(backup, side, ++start));
 		}
 		else
 		{
-			placement(tetri, side, final_str, s);
+			placement(tetri, side, final_str, start);
 			tetri = tetri->next;
 		}
 	}
